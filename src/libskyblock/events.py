a3f2caf5f957300cb55686f0bdbd67ae83e89803
hazyfossa <hazyfossa@gmail.com>
2025-02-17 16:26:17 +0500

    Add support for querying current bingo event

--- src/libskyblock/events.py
+++ src/libskyblock/events.py
@@ -1,4 +1,5 @@
-from libskyblock.api import Api
+from enum import Enum
+from libskyblock.api import Api, BaseResponseModel
 from msgspec import Struct
 
 
@@ -10,9 +11,38 @@ class Firesale(Struct):
     price: int
 
 
+class BingoModifier(Enum):
+    NORMAL = "NORMAL"
+    EXTREME = "EXTREME"
+    SECRET = "SECRET"
+
+
+class BingoGoal(Struct):
+    id: str
+    name: str
+    lore: str
+    fullLore: list  # TODO: stricter type
+
+    tiers: list[int]
+    progress: int
+    requiredAmount: int
+
+
+class Bingo(BaseResponseModel):
+    id: int
+    name: str
+    start: int
+    end: int
+    modifier: BingoModifier
+    goals: list[BingoGoal]
+
+
 class Events:
     def __init__(self, api: Api) -> None:
         self.client = api
 
     def fire_sales(self) -> list[Firesale]:
         return self.client.query("/firesales", model=list[Firesale])
+
+    def current_bingo(self) -> Bingo:
+        return self.client.query("/bingo", model=Bingo)
