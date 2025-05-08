1d5254e0a7deadc19fed4de04662b23337b1b065
hazyfossa <hazyfossa@gmail.com>
2025-05-08 20:13:41 +0500

    Add support for profile querying

--- src/libskyblock/__init__.py
+++ src/libskyblock/__init__.py
@@ -1,12 +1,15 @@
 from pathlib import Path
+
 from .api import Api
 from .bazaar import Bazaar
 from .data.store import DataStore
+from .profiles import Profiles
 from .util.lazy_init import Lazy
 
 
 class Skyblock:
     bazaar = Lazy(Bazaar)
+    profiles = Lazy(Profiles)
 
     def __init__(self, data_path: str | Path, key: str = "-") -> None:
         self.data = DataStore(Path(data_path))
---
+++ src/libskyblock/profiles.py
@@ -0,0 +1,51 @@
+from typing import Literal
+from msgspec import field, Struct
+from libskyblock.types import ProfileID, PlayerID
+from libskyblock.util.lazy_init import Module
+
+
+class Member(Struct):
+    player_id: PlayerID
+    # TODO: support deletion notice
+
+
+class Transaction(Struct):
+    timestamp: int # TODO: datetime
+    amount: int
+    action: Literal["DEPOSIT", "WITHDRAW"] # TODO:verify withdraw
+    initiator_name: str # TODO: PlayerID?
+
+
+class Banking(Struct):
+    balance: int
+
+
+class Profile(Struct):
+    id: ProfileID = field(name="profile_id")
+    members: list[Member]
+    profile_name: str = field(name="cute_name")
+    community_upgrades: dict  # TODO
+    
+    _game_mode: Literal["island", "ironman", "bingo"] | None = field(name="game_mode")
+    @property
+    def game_mode(self) -> Literal["normal", "ironman", "stranded", "bingo"]: # type: ignore
+        # NOTE: Simpler than the official API naming
+        match self._game_mode:
+            case None:
+                return "normal"
+            case "island":
+                return "stranded"
+
+            case *other: # technically we can specify ironman & bingo here to satisfy typing
+                return other
+
+class MultiProfile(Profile):
+    selected: bool
+
+
+class Profiles(Module):
+    def query_id(self, id: ProfileID) -> Profile:
+        return self.client.query(f"/profile", params={"uuid": id}, model=Profile)
+    
+    def query_player(self, player_id: PlayerID) -> list[MultiProfile]:
+        return self.client.query(f"/profiles", params={"uuid": player_id}, model=list[MultiProfile])
